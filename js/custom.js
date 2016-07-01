767a1e7e7aaf95e7adfee05bbea1cc968fdabdd5
Alex Espinoza <aespinoza123@gmail.com>
2016-07-01 13:43:16 -0500

    map fixed

--- js/custom.js
+++ js/custom.js
@@ -129,13 +129,13 @@ $(function() {
   	return false;
   });
 
-  var map;
-        function initMap() {
-          map = new google.maps.Map(document.getElementById('map-canvas'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-          });
-        }
+  // var map;
+  //       function initMap() {
+  //         map = new google.maps.Map(document.getElementById('map-canvas'), {
+  //           center: {lat: -34.397, lng: 150.644},
+  //           zoom: 8
+  //         });
+  //       }
 /* ---------------------------------------------- /*
      * E-mail validation
     /* ---------------------------------------------- */
